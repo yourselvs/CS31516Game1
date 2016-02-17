fac00d90af6a8a75aad1b8dfa38344e23d3051ac
Michael <yourselvs@live.com>
2016-02-17 14:29:41 -0500

    Update Chunk.java
    Changed value to symbol, now represented by a char instead of an int.

--- src/Chunk.java
+++ src/Chunk.java
@@ -1,17 +1,17 @@
 public class Chunk {
-	private int value;
+	private char symbol;
 	
 	/**
 	 * Creates a chunk with a specific value.
 	 * 
 	 * @param value		the value of the chunk
 	 */
-	public Chunk(int value){
-		this.value = value;
+	public Chunk(char c){
+		this.symbol = c;
 	}
 	
 	/**
 	 * @return		returns the value of the chunk
 	 */
-	public int getValue(){return value;}
+	public char getSymbol(){return symbol;}
 }
